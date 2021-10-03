f47c6e9bc22bee629616e775b5a68d548a92f447
Bradley Showen <bshowen@live.seminolestate.edu>
2021-10-03 19:24:10 -0400

    adds ability to click customer rows

--- Views/customer.php
+++ Views/customer.php
@@ -4,9 +4,30 @@ $page = new Page();
 
 $db = new Database();
 
-$customer_id = intval($_GET['id']);
+
+$query = "SELECT * FROM Customers WHERE customer_id = ?";
+$params = ['id' => intval($_GET['id'])];
+$result = $db->execute_sql_statement($query, $params);
+if($result[0]){
+  $result = ($result[1])->fetch_assoc();
+  extract($result);
+}
 ?>
 
 <main>
-  <p>Customer page. Customer is = <?php  echo $customer_id; ?></p>
+  <div class="customer_card">
+    <h1><?php echo $first_name." ".$last_name; ?></h1>
+    <p><?php echo $street_address;?></p>
+    <p><?php echo $city;?></p>
+    <p><?php echo $state; ?></p>
+    <p><?php echo $zip; ?></p>
+  </div>
+
+  <!-- <div class="customer_sales">
+    <p>Customer sales here</p>
+  </div>
+
+  <div class="customer_invoices">
+    <p>Customer invoices here</p>
+  </div> -->
 </main>
